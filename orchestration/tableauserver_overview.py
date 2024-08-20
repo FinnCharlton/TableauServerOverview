77f0d2b4fc3a123f7ba5e927a28622ab46b5c1c4
FinnCharlton <finn.charlton@hotmail.com>
2024-08-20 13:47:45 +0100

    Added comments to Airflow DAG definition

--- orchestration/tableauserver_overview.py
+++ orchestration/tableauserver_overview.py
@@ -3,6 +3,8 @@ from airflow.operators.bash import BashOperator
 import datetime
 
 with DAG(
+
+    #Define DAG name and run schedule
     dag_id='tableauserver_overview',
     start_date=datetime.datetime(2024, 8, 19),
     schedule_interval='@daily',
@@ -10,14 +12,18 @@ with DAG(
     tags=['prod','python','dbt'],
 
 ) as dag:
+    
+    #Task 1 runs the ingestion script
     task_1 = BashOperator(
     task_id='ingestion_task',
     bash_command="python /opt/airflow/dags/src/main.py"
     )
 
+    #Task 2 navigates to the dbt project folder and runs the build command, specifying the location of profiles.yml
     task_2 = BashOperator(
     task_id='modelling_task',
     bash_command="cd /opt/airflow/plugins/tableauserver_overview && dbt build --profiles-dir ..",
     )
 
+    #Task 2 specified as downstream of Task 1
     task_1 >> task_2
